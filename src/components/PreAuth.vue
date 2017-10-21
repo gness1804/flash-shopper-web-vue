2d4aa677444156aa846cdd47845d8d454d5aed0e
gness1804 <graham.nessler@gmail.com>
2017-10-21 17:14:52 -0500

    Create ability to user to sign up

--- src/components/PreAuth.vue
+++ src/components/PreAuth.vue
@@ -11,10 +11,19 @@
       placeholder="Enter Your Password"
       v-model="password"
     />
+    <div class="buttons">
+      <button
+        v-on:click="signUp"
+      >
+        Sign Up
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import * as firebase from 'firebase';
+
 export default {
   name: 'PreAuth',
   data() {
@@ -22,6 +31,30 @@ export default {
       email: '',
       password: '',
     };
+  },
+  methods: {
+    resetEmailAndPWStates: function () {
+      this.email = '';
+      this.password = '';
+    },
+    showSignUpError: function () {
+      alert('There was an error with authentication. Please check your credentials and try again.');
+    },
+    signUp: function () {
+      const { email, password } = this;
+      if (!email || !password) {
+        alert('Oops! You need both an email address and a password to sign up. Please try again.');
+        return;
+      }
+      const promise = new Promise((resolve) => {
+        resolve(
+          firebase.auth().createUserWithEmailAndPassword(email, password),
+        );
+      });
+      promise
+        .then(() => { this.resetEmailAndPWStates(); })
+        .catch(() => { this.showSignUpError(); });
+    },
   },
 };
 </script>
