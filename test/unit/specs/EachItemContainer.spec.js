8f3d7c9c31f1269cfb43a5f217232c7c13f1303c
gness1804 <graham.nessler@gmail.com>
2017-10-27 13:23:23 -0500

    Add test for should not display the inCart styling by default

--- test/unit/specs/EachItemContainer.spec.js
+++ test/unit/specs/EachItemContainer.spec.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import { mount } from 'avoriaz';
 import EachItemContainer from '@/components/EachItemContainer';
 import item from '../helpers/FakeItem';
 
@@ -16,5 +17,13 @@ describe('EachItemContainer.vue', () => {
       .to.equal('Aisle:10');
     expect(vm.$el.querySelector('.each-item-container .each-item-note').textContent)
       .to.equal('\n    Note:\n    \n    Please do not get stale bread\n  ');
+  });
+
+  it('should not display the inCart styling by default', () => {
+    const component = mount(EachItemContainer, { propsData });
+    const el = component.find('.each-item-name');
+    expect(el.length).to.equal(1);
+    const el2 = component.find('.each-item-name.strike');
+    expect(el2.length).to.equal(0);
   });
 });
