e4c1096d1a8cb2661393c6e955a78d15ffa2302c
gness1804 <graham.nessler@gmail.com>
2017-10-26 20:08:54 -0500

    Add first test for EachItemContainer

---
+++ test/unit/specs/EachItemContainer.spec.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import EachItemContainer from '@/components/EachItemContainer';
+import fakeItem from '../helpers/FakeItem';
+
+describe('EachItemContainer.vue', () => {
+  const Constructor = Vue.extend(EachItemContainer);
+  const propsData = {
+    item: fakeItem,
+  };
+
+  it('should render correct contents', () => {
+    const vm = new Constructor({ propsData }).$mount();
+    expect(vm.$el.querySelector('.each-item-container .each-item-name').textContent.trim())
+      .to.equal('Foo bread');
+  });
+});
