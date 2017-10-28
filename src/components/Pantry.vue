deb583bf1451a0ce256bce827c0b9e22ad92db27
gness1804 <graham.nessler@gmail.com>
2017-10-28 15:28:51 -0500

    Add pantry route

---
+++ src/components/Pantry.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="pantry">
+    <p>I am the pantry component.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
+import Pantry from './components/Pantry';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,10 @@ const routes = [
   {
     path: '/',
     component: App,
+  },
+  {
+    path: '/pantry',
+    component: Pantry,
   },
 ];
 
