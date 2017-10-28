b15045045525f45fc426854f8047ecf6a9369aa0
gness1804 <graham.nessler@gmail.com>
2017-10-28 16:10:03 -0500

    Hook up firebase to Pantry

--- src/components/Pantry.vue
+++ src/components/Pantry.vue
@@ -7,15 +7,82 @@
     >
       Go Home
     </button>
+    <div
+      class="pantry-main"
+      v-if="isUser"
+    >
+    <!-- all the stuff goes here -->
+    <p>I should only show up if there is a user.</p>
+    </div>
   </div>
 </template>
 
 <script>
+import * as firebase from 'firebase';
+import firebaseApp from '../../firebaseConfig';  // eslint-disable-line
+import cleanUpUserEmail from '../helpers/cleanUpUserEmail';
+
 export default {
+  name: 'Pantry',
+  data() {
+    return {
+      isUser: false,
+      itemsRef: {},
+      userEmail: '',
+      userId: null,
+      items: [],
+    };
+  },
   methods: {
     goHome: function () {
       this.$router.push('/');
     },
+    initializeApp: function () {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.isUser = true;
+          const email = cleanUpUserEmail(user.email);
+          this.userEmail = user.email;
+          this.userId = user.uid;
+          this.itemsRef = firebase.database().ref(email + '/pantry') //eslint-disable-line
+          this.listenForItems(this.itemsRef);
+        } else {
+          this.isUser = false;
+        }
+      });
+    },
+    listenForItems: function (itemsRef) {
+      itemsRef.on('value', (snapshot) => {
+        const newArr = [];
+        snapshot.forEach((item) => {
+          newArr.push({
+            name: item.val().name,
+            aisle: item.val().aisle,
+            quantity: item.val().quantity,
+            note: item.val().note,
+            inCart: item.val().inCart || false,
+            id: item.key,
+          });
+        });
+        this.sortItems(newArr);
+      });
+    },
+    sortItems: function (_items) {
+      this.items = _items.sort((a, b) => {
+        const first = a.name.toLowerCase();
+        const second = b.name.toLowerCase();
+        if (first < second) {
+          return -1;
+        }
+        if (first > second) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+  },
+  mounted: function () {
+    this.initializeApp();
   },
 };
 </script>
