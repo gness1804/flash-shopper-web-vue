2a890b518a264cb5b05354bec52be08d244351dd
gness1804 <graham.nessler@gmail.com>
2017-11-04 13:14:23 -0500

    Add tests for thereAreItemsInCart helper function

---
+++ test/unit/specs/thereAreItemsInCart.spec.js
@@ -0,0 +1,42 @@
+import { assert } from 'chai';
+import thereAreItemsInCart from '../../../src/helpers/thereAreItemsInCart';
+import items from '../helpers/FakeItemsArray';
+
+describe('thereAreItemsInCart', () => {
+  const inCartItems = items;
+  const noInCartItems = [
+    {
+      id: '1.5',
+      name: 'Candy',
+      aisle: '15',
+      note: 'No Junior Mints!',
+      quantity: '3 boxes',
+      inCart: false,
+    },
+    {
+      id: '2.5',
+      name: 'Brown rice',
+      aisle: '11',
+      note: '',
+      quantity: '2 boxes',
+      inCart: false,
+    },
+  ];
+  const emptyItems = [];
+
+  it('should be a function', () => {
+    assert.isFunction(thereAreItemsInCart);
+  });
+
+  it('should return true if one or more items is in cart', () => {
+    assert.strictEqual(thereAreItemsInCart(inCartItems), true);
+  });
+
+  it('should return false if no items are in cart', () => {
+    assert.strictEqual(thereAreItemsInCart(noInCartItems), false);
+  });
+
+  it('should return false if items is an empty array', () => {
+    assert.strictEqual(thereAreItemsInCart(emptyItems), false);
+  });
+});
