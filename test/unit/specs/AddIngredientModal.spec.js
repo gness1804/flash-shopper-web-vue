48d37ce423ec0167cf379912b7866e5370cf7368
gness1804 <graham.nessler@gmail.com>
2017-11-05 18:14:56 -0600

    Add test for clicking the add ingredient button should trigger the addIngredient method

--- test/unit/specs/AddIngredientModal.spec.js
+++ test/unit/specs/AddIngredientModal.spec.js
@@ -7,4 +7,13 @@ describe('AddIngredientModal', () => {
     const header = component.find('h2')[0];
     expect(header.text()).to.equal('Add Ingredient');
   });
+
+  it('clicking the add ingredient button should trigger the addIngredient method', () => {
+    const component = mount(AddIngredientModal);
+    const addIngredient = sinon.stub();
+    component.setMethods({ addIngredient });
+    const button = component.find('.add-ingredient-modal-button')[0];
+    button.trigger('click');
+    expect(addIngredient.calledOnce).to.equal(true);
+  });
 });
