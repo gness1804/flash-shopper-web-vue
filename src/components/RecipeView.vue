13f843a9176f4e16cb2435ee30486a93764fa5cd
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2017-11-17 16:46:22 -0600

    Hook up firebase recipe data

--- src/components/RecipeView.vue
+++ src/components/RecipeView.vue
@@ -1,21 +1,86 @@
 <template>
   <div class="recipe-view">
-    <p>I am the recipe view component.</p>
+    <h2>{{title}}</h2>
   </div>
 </template>
 
 <script>
+// @flow
+
+import * as firebase from 'firebase';
+import firebaseApp from '../../firebaseConfig';  // eslint-disable-line
+import Item from '../models/Item';
+import Recipe from '../models/Recipe';
+import cleanUpUserEmail from '../helpers/cleanUpUserEmail';
+
 export default {
   name: 'recipeView',
   data(): {
     id: string,
+    title: string,
+    image: string,
+    ingredients: Array<Item>,
+    directions: Array<string>,
+    isUser: boolean,
+    userEmail: string,
+    userId: string,
+    itemsRef: Object,
   } {
     return {
       id: '',
+      title: '',
+      image: '',
+      ingredients: [],
+      directions: [],
+      isUser: false,
+      userEmail: '',
+      userId: '',
+      itemsRef: {},
     };
+  },
+  methods: {
+    filterOutTargetRecipe: function (recipes: Array<Recipe>): void {
+      const target = recipes.filter((rec: Recipe) => {
+        return rec.id === this.id;
+      });
+      this.title = target[0].title;
+      this.image = target[0].image;
+      this.ingredients = target[0].ingredients;
+      this.directions = target[0].directions;
+    },
+    initializeApp: function (): void {
+      firebase.auth().onAuthStateChanged((user: Object) => {
+        if (user) {
+          this.isUser = true;
+          const email = cleanUpUserEmail(user.email);
+          this.userEmail = user.email;
+          this.userId = user.uid;
+          this.itemsRef = firebase.database().ref(email + '/recipes') //eslint-disable-line
+          this.listenForItems(this.itemsRef);
+        } else {
+          this.isUser = false;
+        }
+      });
+    },
+    listenForItems: function (itemsRef: Object): void {
+      itemsRef.on('value', (snapshot: Array<Object>) => {
+        const newArr = [];
+        snapshot.forEach((recipe: Object) => {
+          newArr.push({
+            title: recipe.val().title,
+            image: recipe.val().image,
+            ingredients: recipe.val().ingredients,
+            directions: recipe.val().directions,
+            id: recipe.key,
+          });
+        });
+        this.filterOutTargetRecipe(newArr);
+      });
+    },
   },
   mounted: function (): void {
     this.id = this.$route.params.id;
+    this.initializeApp();
   },
 };
 </script>
