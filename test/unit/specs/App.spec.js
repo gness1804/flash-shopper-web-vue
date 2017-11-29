53a32f45030743dfde3dd144a7d7d13561debdf3
gness1804 <graham.nessler@gmail.com>
2017-11-28 20:07:22 -0600

    Add buttonStrings helper

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -13,8 +13,10 @@ describe('App.vue', () => {
     component.setData({ userEmail });
     const headline = component.find('h1')[0];
     const message = component.find('.user-logged-in-message')[0];
+    const logOutText = component.find('.log-out-button')[0];
     expect(headline.text()).to.equal('Flash Shopper');
     expect(message.text().trim()).to.equal('Logged in as foo@foo.com');
+    expect(logOutText.text().trim()).to.equal('Log Out');
   });
 
   it('should trigger the logOut method when user clicks the log out button', () => {
