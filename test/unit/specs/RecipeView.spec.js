a3de48f2f61b45bc01589b69f18111f3dace806b
gness1804 <graham.nessler@gmail.com>
2017-11-29 20:06:19 -0600

    Finish adding button strings

--- test/unit/specs/RecipeView.spec.js
+++ test/unit/specs/RecipeView.spec.js
@@ -10,4 +10,17 @@ describe('RecipeView.vue', () => {
     const el = component.find('.recipe-view-headline')[0];
     expect(el.text().trim()).to.equal('Pumpkin pie');
   });
+
+  it('should render button string data correctly ', () => {
+    const component = mount(RecipeView);
+    component.setData({ isUser: true });
+    const el1 = component.find('.remove-image-button')[0];
+    expect(el1.text().trim()).to.equal('Remove Image');
+    const el2 = component.find('.add-ingredient-button')[0];
+    expect(el2.text().trim()).to.equal('Add Ingredient');
+    const el3 = component.find('.add-direction-button')[0];
+    expect(el3.text().trim()).to.equal('Add Direction');
+    const el4 = component.find('.go-home-button')[0];
+    expect(el4.text().trim()).to.equal('Go Home');
+  });
 });
