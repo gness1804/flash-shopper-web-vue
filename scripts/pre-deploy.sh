40b363d49cb659c15c86f1b5198c21b5e2b32f52
gness1804 <graham.nessler@gmail.com>
2017-11-30 19:28:37 -0600

    Deploy to gh pages

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -10,14 +10,12 @@ fi
 rm -rf dist/
 git merge master
 
-# gets rid of build-related script tags in index.html
-grep -v "<script" index.html > index2.html
-mv index2.html index.html
+# grep -v "<script" index.html > index2.html
+# mv index2.html index.html
 
-# gets rid of build-related link (css) tags in index.html
-grep -v "<link href=dist/static/css/" index.html > index2.html
-mv index2.html index.html
-grep -v "<link href=/static/css/" index.html > index2.html
-mv index2.html index.html
+# grep -v "<link href=dist/static/css/" index.html > index2.html
+# mv index2.html index.html
+# grep -v "<link href=/static/css/" index.html > index2.html
+# mv index2.html index.html
 
 echo "Now ready to test locally."
