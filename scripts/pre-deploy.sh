318ddd481a11db18ebea552b4edb619bda6de7ab
gness1804 <graham.nessler@gmail.com>
2017-12-04 19:56:05 -0600

    Fix pre-deploy script to match gh-pages

--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -10,14 +10,10 @@ fi
 rm -rf dist/
 git merge master
 
-# gets rid of build-related script tags in index.html
-grep -v "<script" index.html > index2.html
-mv index2.html index.html
-
-# gets rid of build-related link (css) tags in index.html
-grep -v "<link href=dist/static/css/" index.html > index2.html
-mv index2.html index.html
-grep -v "<link href=/static/css/" index.html > index2.html
-mv index2.html index.html
+# get rid of the build files
+sed -i -e 's/<link href=dist\/static\/css\/app.[a-z|A-Z|0-9]*.css rel=stylesheet>//g' index.html
+sed -i -e 's/<script type=text\/javascript src=dist\/static\/js\/manifest.[a-z|A-Z\0-9]*.js><\/script>//g' index.html
+sed -i -e 's/<script type=text\/javascript src=dist\/static\/js\/vendor.[a-z|A-Z|0-9]*.js><\/script>//g' index.html
+sed -i -e 's/<script type=text\/javascript src=dist\/static\/js\/app.[a-z|A-Z\0-9]*.js><\/script>//g' index.html
 
 echo "Now ready to test locally."
