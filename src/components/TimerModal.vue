a3f0e5b57659f6a3d0039c685fd4e76117402903
gness1804 <graham.nessler@gmail.com>
2017-12-06 18:56:33 -0600

    Add stopwatch functionality

--- src/components/TimerModal.vue
+++ src/components/TimerModal.vue
@@ -9,6 +9,23 @@
     </p>
     <p>Do you want to:</p>
     <!-- radio select area with two choices: "Start Stopwatch" or "Count Down."-->
+    <div
+      class="stopwatch-container"
+    >
+      <a
+        class="link stopwatch-link"
+        v-bind:href="stopwatchLink"
+        target="_blank"
+      >
+        Start Stopwatch
+      </a>
+      <img
+        class="large-icon stopwatch-icon"
+        src="../assets/stopwatch.png"
+        v-on:click="startStopwatch"
+        title="Start Stopwatch"
+          />
+    </div>
     <!-- if stopwatch is chosen, a new window opens with the Google search for "start stopwatch." -->
     <!-- if count down is chosen, a hidden pair of dropdwns appears: one for number (1-60 in increments of 5) and the other for unit (seconds, minutes, hours) -->
     <!-- the user can then click on a button in this hidden area go to go a Google search for "set timer for ${time} ${units}" -->
@@ -20,9 +37,17 @@
 
 export default {
   name: 'timerModal',
+  data() {
+    return {
+      stopwatchLink: 'https://www.google.com/search?q=start+stopwatch',
+    };
+  },
   methods: {
     closeModal: function (): void {
       this.$emit('closeTimerModal');
+    },
+    startStopwatch: function (): void {
+      window.open('https://www.google.com/search?q=start+stopwatch');
     },
   },
 };
@@ -35,6 +60,20 @@ export default {
 
   .close-modal-button:hover {
     cursor: pointer;
+  }
+
+  .stopwatch-container {
+    align-items: center;
+    border: 1px solid #000000;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 40px auto;
+    padding: 10px;
+  }
+
+  .stopwatch-link {
+    margin-right: 20px;
   }
 </style>
 
--- test/unit/specs/TimerModal.spec.js
+++ test/unit/specs/TimerModal.spec.js
@@ -10,4 +10,13 @@ describe('TimerModal', () => {
     button.trigger('click');
     expect(closeModal.calledOnce).to.equal(true);
   });
+
+  it('clicking the start stopwatch button should trigger the startStopwatch method', () => {
+    const component = mount(TimerModal);
+    const startStopwatch = sinon.stub();
+    component.setMethods({ startStopwatch });
+    const button = component.find('.stopwatch-icon')[0];
+    button.trigger('click');
+    expect(startStopwatch.calledOnce).to.equal(true);
+  });
 });
