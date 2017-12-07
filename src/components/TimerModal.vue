238a3e95139c6a220482408caa745db304f5dbfa
gness1804 <graham.nessler@gmail.com>
2017-12-06 19:13:29 -0600

    Add time dropdown

--- src/components/TimerModal.vue
+++ src/components/TimerModal.vue
@@ -26,7 +26,32 @@
         title="Start Stopwatch"
           />
     </div>
-    <!-- if stopwatch is chosen, a new window opens with the Google search for "start stopwatch." -->
+    <div
+      class="timer-container"
+    >
+      <p
+        class="count-from-text"
+      >
+        Count down from:
+      </p>
+      <select
+        class="timer-numbers-select"
+        v-model="time"
+      >
+        <option>5</option>
+        <option>10</option>
+        <option>15</option>
+        <option>20</option>
+        <option>25</option>
+        <option>30</option>
+        <option>35</option>
+        <option>40</option>
+        <option>45</option>
+        <option>50</option>
+        <option>55</option>
+        <option>60</option>
+      </select>
+    </div>
     <!-- if count down is chosen, a hidden pair of dropdwns appears: one for number (1-60 in increments of 5) and the other for unit (seconds, minutes, hours) -->
     <!-- the user can then click on a button in this hidden area go to go a Google search for "set timer for ${time} ${units}" -->
   </div>
@@ -37,8 +62,12 @@
 
 export default {
   name: 'timerModal',
-  data() {
+  data(): {
+    time: string,
+    stopwatchLink: string,
+  } {
     return {
+      time: '',
       stopwatchLink: 'https://www.google.com/search?q=start+stopwatch',
     };
   },
@@ -62,7 +91,8 @@ export default {
     cursor: pointer;
   }
 
-  .stopwatch-container {
+  .stopwatch-container,
+  .timer-container {
     align-items: center;
     border: 1px solid #000000;
     display: flex;
