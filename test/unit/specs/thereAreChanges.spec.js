fd45cb8db948636834e21f2ae5792c0cc4ca93e8
gness1804 <graham.nessler@gmail.com>
2017-12-17 16:40:57 -0600

    Add more tests for thereAreChanges

--- test/unit/specs/thereAreChanges.spec.js
+++ test/unit/specs/thereAreChanges.spec.js
@@ -20,4 +20,24 @@ describe('thereAreChanges', () => {
   it('should return false if there are no changes', () => {
     assert.strictEqual(thereAreChanges(options), false);
   });
+
+  it('should return true if name differs from initName', () => {
+    const options2 = { ...options, name: 'Light Cream' };
+    assert.strictEqual(thereAreChanges(options2), true);
+  });
+
+  it('should return true if aisle differs from initAisle', () => {
+    const options2 = { ...options, aisle: '' };
+    assert.strictEqual(thereAreChanges(options2), true);
+  });
+
+  it('should return true if note differs from initNote', () => {
+    const options2 = { ...options, note: 'Avoid off brand.' };
+    assert.strictEqual(thereAreChanges(options2), true);
+  });
+
+  it('should return true if quantity differs from initQty', () => {
+    const options2 = { ...options, quantity: '12 oz' };
+    assert.strictEqual(thereAreChanges(options2), true);
+  });
 });
