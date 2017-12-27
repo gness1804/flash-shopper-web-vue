6c152210e822376b557b85df211fae39367fdf4b
gness1804 <graham.nessler@gmail.com>
2017-12-27 14:48:25 -0500

    Add test for clicking the clear note button should clear the note

--- test/unit/specs/NoteModal.spec.js
+++ test/unit/specs/NoteModal.spec.js
@@ -21,8 +21,11 @@ describe('NoteModal', () => {
     expect(closeModal.calledOnce).to.equal(true);
   });
 
-  // it('clicking the clear note button should clear the note', () => {
-  //   const component = mount(NoteModal, { propsData });
-
-  // });
+  it('clicking the clear note button should clear the note', () => {
+    const component = mount(NoteModal, { propsData });
+    expect(component.data().noteString).to.equal('One can substitute honey for sugar.');
+    const button = component.find('.clear-note-button')[0];
+    button.trigger('click');
+    expect(component.data().noteString).to.equal('');
+  });
 });
