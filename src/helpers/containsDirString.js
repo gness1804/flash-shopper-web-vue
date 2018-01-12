10e5d00d08767903b4a2487f557565068f7debf3
gness1804 <graham.nessler@gmail.com>
2018-01-11 18:19:32 -0600

    Fix casing problem with direction match

--- src/helpers/containsDirString.js
+++ src/helpers/containsDirString.js
@@ -1,7 +1,7 @@
 // @flow
 
 const containsDirString = (dir: string, name: string): boolean => {
-  if (dir.includes(name)) {
+  if (dir.toLowerCase().includes(name.toLowerCase())) {
     return true;
   }
   return false;
