d5e7c05726092b9563dd537781aa0da4df012422
gness1804 <graham.nessler@gmail.com>
2018-01-21 15:37:04 -0600

    Add better microinteraction when adding pantry item to main list

--- src/components/EachPantryItem.vue
+++ src/components/EachPantryItem.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="each-pantry-item">
+  <div
+    class="each-pantry-item"
+    v-bind:class="{ highlighted: isHighlighted }"
+  >
     <h3 class="pantry-item-name">{{item.name}}</h3>
     <img
         class="large-icon add-item-to-main-list-button"
@@ -33,6 +36,13 @@ export default {
       required: true,
     },
   },
+  data(): {
+    isHighlighted: boolean,
+  } {
+    return {
+      isHighlighted: false,
+    };
+  },
   methods: {
     deleteItem: function (): void {
       const warning = confirm(`Warning: do you want to delete ${this.item.name} from the pantry? This cannot be undone!`);
@@ -42,6 +52,10 @@ export default {
     },
     transferItemToMainList: function (): void {
       this.$emit('transferItemToMainList', this.item);
+      this.isHighlighted = true;
+      setTimeout(() => {
+        this.isHighlighted = false;
+      }, 3000);
     },
     viewEditModal: function (): void {
       this.$emit('viewEditModal', this.item);
@@ -56,6 +70,11 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: center;
+  }
+
+  .highlighted {
+    border: 2px solid #f00;
+    background-color: #C56415;
   }
 
   .pantry-item-name {
