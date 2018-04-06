8f1e0fe1b08f1b8966a1c05f687f90b252ed7e77
gness1804 <graham.nessler@gmail.com>
2018-04-05 21:07:53 -0500

    Flesh out Completed a little

--- src/components/Completed.vue
+++ src/components/Completed.vue
@@ -1,14 +1,41 @@
 <template>
   <div id="completed">
+    <app-header
+      v-bind:isUser="isUser"
+      v-bind:userEmail="userEmail"
+      v-on:logOut="logOut"
+    >
+    </app-header>
     <p>I am the completed items.</p>
   </div>
 </template>
 
 <script>
 // @flow
+import AppHeader from './AppHeader';
+import logOut from '../helpers/logOut';
+
+interface Data {
+  isUser: boolean,
+  userEmail: string,
+}
 
 export default {
   name: 'completed',
+  components: {
+    AppHeader,
+  },
+  data(): Data {
+    return {
+      isUser: false,
+      userEmail: '',
+    };
+  },
+  methods: {
+    logOut: function (): void {
+      logOut();
+    },
+  },
 };
 </script>
 
