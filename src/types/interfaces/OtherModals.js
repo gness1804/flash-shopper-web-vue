453856b70ed99f42732325113789e6adc45a2336
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-04-13 20:55:42 -0400

    Add AddSourceInt

--- src/types/interfaces/OtherModals.js
+++ src/types/interfaces/OtherModals.js
@@ -4,3 +4,10 @@ export interface NoteModalInt {
   noteString: string,
   saveString: string,
 }
+
+export interface AddSourceInt {
+  newSource: string,
+  saveString: string,
+  cancelString: string,
+  clearString: string,
+}
