64f93447ef75bef3c19a3be7176fbb25499d9188
Graham Nessler <grahamnessler@IRXWMKCHV2J.hsd1.va.comcast.net>
2018-04-15 17:30:44 -0400

    Add tests for sortByTimesMadeHelper

--- src/helpers/sortByTimesMadeHelper.js
+++ src/helpers/sortByTimesMadeHelper.js
@@ -6,7 +6,9 @@ const sortByTimesMadeHelper = (recipes: Recipe[]): Recipe[] => {
     return [];
   }
   return recipes.sort((a: Recipe, b: Recipe) => {
-    return b.timesMade - a.timesMade;
+    const first = a.timesMade || 0;
+    const second = b.timesMade || 0;
+    return second - first;
   });
 };
 
