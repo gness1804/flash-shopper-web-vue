3859f415dcd097361f7c55694167eed424336b0a
gness1804 <graham.nessler@gmail.com>
2018-12-02 15:56:42 -0600

    Add ping to website every 5 mins on prod

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import http from 'http';
 import router from './routes';
 
+// ping the app every five minutes to keep Heroku dynos awake (prod only)
+if (process.env.NODE_ENV === 'production') {
+  setInterval(() => {
+    http.get('https://flash-shopper-web.herokuapp.com');
+  }, 300000); // every 5 minutes (300000)
+}
+
+// start the app
 /* eslint-disable no-new */
 new Vue({
   router,
