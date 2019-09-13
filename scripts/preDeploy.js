81aa5257edae7d4e35d07b4228a975890fd3261f
gness1804 <graham.nessler@gmail.com>
2019-09-13 15:20:43 -0400

    Add more thorough info message to preCommit script

--- scripts/preDeploy.js
+++ scripts/preDeploy.js
@@ -4,4 +4,10 @@ const handleOutput = require('./handleOutput');
 execSync('rm -rf dist', handleOutput);
 execSync('npm run build', handleOutput);
 
-console.info('Project built! See https://medium.com/@mwolfhoffman/deploying-to-github-pages-with-vue-webpack-cli-5b2ba17f14a0 for more details on finishing the deploy.');
+console.info(`Project built!
+  To finish the deploy, replace the following files in the root 'index.html' file with the files referenced in the 'index.html' in the 'dist' dir:
+  * '<link href=dist/static/css/app.[custom string].css rel=stylesheet>'
+  * '<script type=text/javascript src=dist/static/js/manifest.[custom string].js> </script>'
+  * '<script type=text/javascript src=dist/static/js/vendor.[custom string].js> </script>'
+  * '<script type=text/javascript src=dist/static/js/app.[custom string].js> </script>'
+For more details, see https://medium.com/@mwolfhoffman/deploying-to-github-pages-with-vue-webpack-cli-5b2ba17f14a0`);
